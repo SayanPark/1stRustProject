ce3476dd997a854ae947593546f6993b8462ba53
SayanPark <s.poormanesh2002@gmail.com>
2024-10-09 19:16:12 +0330

    Added new Rust assignment: cli

---
+++ cli/target/debug/cli.d
@@ -0,0 +1,1 @@
+E:\programming\Rust\projects\projects\cli\target\debug\cli.exe: E:\programming\Rust\projects\projects\cli\src\main.rs
---
+++ cli/target/debug/deps/cli.d
@@ -0,0 +1,5 @@
+E:\programming\Rust\projects\projects\cli\target\debug\deps\cli.exe: src/main.rs
+
+E:\programming\Rust\projects\projects\cli\target\debug\deps\cli.d: src/main.rs
+
+src/main.rs:
