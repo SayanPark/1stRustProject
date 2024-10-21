4b4c69399b38b4379b557cd5ac110cd3f7272378
SayanPark <s.poormanesh2002@gmail.com>
2024-10-21 14:33:12 +0330

    new file:   message/Cargo.lock 	new file:   message/Cargo.toml 	new file:   message/src/main.rs 	new file:   message/target/.rustc_info.json 	new file:   message/target/CACHEDIR.TAG 	new file:   message/target/debug/.cargo-lock 	new file:   message/target/debug/.fingerprint/message-e351715097933df0/bin-message 	new file:   message/target/debug/.fingerprint/message-e351715097933df0/bin-message.json 	new file:   message/target/debug/.fingerprint/message-e351715097933df0/dep-bin-message 	new file:   message/target/debug/.fingerprint/message-e351715097933df0/invoked.timestamp 	new file:   message/target/debug/deps/message.d 	new file:   message/target/debug/deps/message.exe 	new file:   message/target/debug/deps/message.pdb 	new file:   message/target/debug/incremental/message-1i542f1j290pf/s-h1259r2nc4-09tm4xs-8w8cp9cifdhbds1ywclht3ad5/17jsu06l2e7j7t17jyq7b7i2l.o 	new file:   message/target/debug/incremental/message-1i542f1j290pf/s-h1259r2nc4-09tm4xs-8w8cp9cifdhbds1ywclht3ad5/27daojuimb40ur72ncflpjtz5.o 	new file:   message/target/debug/incremental/message-1i542f1j290pf/s-h1259r2nc4-09tm4xs-8w8cp9cifdhbds1ywclht3ad5/b10cp0gm3rexry83ewclo7l7u.o 	new file:   message/target/debug/incremental/message-1i542f1j290pf/s-h1259r2nc4-09tm4xs-8w8cp9cifdhbds1ywclht3ad5/dep-graph.bin 	new file:   message/target/debug/incremental/message-1i542f1j290pf/s-h1259r2nc4-09tm4xs-8w8cp9cifdhbds1ywclht3ad5/dkkbi5wqq7agevro4sbc0vtia.o 	new file:   message/target/debug/incremental/message-1i542f1j290pf/s-h1259r2nc4-09tm4xs-8w8cp9cifdhbds1ywclht3ad5/drt55u7o0q9teg312cylp3kpf.o 	new file:   message/target/debug/incremental/message-1i542f1j290pf/s-h1259r2nc4-09tm4xs-8w8cp9cifdhbds1ywclht3ad5/epqhe7gnvf9c5eg79c8txdm3w.o 	new file:   message/target/debug/incremental/message-1i542f1j290pf/s-h1259r2nc4-09tm4xs-8w8cp9cifdhbds1ywclht3ad5/query-cache.bin 	new file:   message/target/debug/incremental/message-1i542f1j290pf/s-h1259r2nc4-09tm4xs-8w8cp9cifdhbds1ywclht3ad5/work-products.bin 	new file:   message/target/debug/incremental/message-1i542f1j290pf/s-h1259r2nc4-09tm4xs.lock 	new file:   message/target/debug/message.d 	new file:   message/target/debug/message.exe 	new file:   message/target/debug/message.pdb

---
+++ message/target/debug/deps/message.d
@@ -0,0 +1,5 @@
+E:\programming\Rust\projects\projects\message\target\debug\deps\message.exe: src/main.rs
+
+E:\programming\Rust\projects\projects\message\target\debug\deps\message.d: src/main.rs
+
+src/main.rs:
---
+++ message/target/debug/message.d
@@ -0,0 +1,1 @@
+E:\programming\Rust\projects\projects\message\target\debug\message.exe: E:\programming\Rust\projects\projects\message\src\main.rs
