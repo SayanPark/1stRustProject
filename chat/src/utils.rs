50e841ebde18e46515cfb7292ba73d590455f965
SayanPark <s.poormanesh2002@gmail.com>
2024-11-18 16:36:36 +0330

    modified:   chat/src/utils.rs 	renamed:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x3safklz-18blngr-working/dep-graph.part.bin -> chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x410p8q4-04u1ezf-working/dep-graph.part.bin 	renamed:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x3safklz-18blngr.lock -> chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x410p8q4-04u1ezf.lock 	new file:   chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x411xru6-16ypti3-working/dep-graph.part.bin 	renamed:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x3safpyb-1lmge95.lock -> chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x411xru6-16ypti3.lock 	new file:   chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x412ydrj-1rs3b1o-working/dep-graph.part.bin 	new file:   chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x412ydrj-1rs3b1o.lock 	new file:   chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x414ctv3-0266z7y-working/dep-graph.part.bin 	new file:   chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x414ctv3-0266z7y.lock 	new file:   chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x415pdug-1f8odbx-working/dep-graph.part.bin 	new file:   chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x415pdug-1f8odbx.lock 	renamed:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x3safpyb-1lmge95-working/dep-graph.part.bin -> chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x410p8q1-0wtogc5-working/dep-graph.part.bin 	new file:   chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x410p8q1-0wtogc5.lock 	new file:   chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x411xmos-0ng1qes-working/dep-graph.part.bin 	new file:   chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x411xmos-0ng1qes.lock 	new file:   chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x412ydrp-0ivbidj-working/dep-graph.part.bin 	new file:   chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x412ydrp-0ivbidj.lock 	new file:   chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x414ct77-11h80k9-working/dep-graph.part.bin 	new file:   chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x414ct77-11h80k9.lock 	new file:   chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x415pduj-09ml1th-working/dep-graph.part.bin 	new file:   chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x415pduj-09ml1th.lock

---
+++ chat/src/utils.rs
@@ -0,0 +1,7 @@
+use async_std::prelude::*;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use std::error::Error;
+use std::marker::Unpin;
+
+pub type ChatError = Box<dyn Error + Send + Sync + 'static>;
