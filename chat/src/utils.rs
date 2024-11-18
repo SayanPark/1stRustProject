7cd9e467e3d579a8275c28b6acc5b6b509655b3b
SayanPark <s.poormanesh2002@gmail.com>
2024-11-18 16:43:11 +0330

    modified:   chat/src/utils.rs 	deleted:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x414ctv3-0266z7y-working/dep-graph.part.bin 	deleted:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x415pdug-1f8odbx-working/dep-graph.part.bin 	renamed:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x410p8q4-04u1ezf-working/dep-graph.part.bin -> chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x47w71v3-01g4nnh-working/dep-graph.part.bin 	renamed:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x410p8q4-04u1ezf.lock -> chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x47w71v3-01g4nnh.lock 	renamed:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x411xru6-16ypti3-working/dep-graph.part.bin -> chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x47zeqca-016u5ej-working/dep-graph.part.bin 	renamed:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x411xru6-16ypti3.lock -> chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x47zeqca-016u5ej.lock 	renamed:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x412ydrj-1rs3b1o-working/dep-graph.part.bin -> chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x481do3y-1rpfqs8-working/dep-graph.part.bin 	renamed:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x412ydrj-1rs3b1o.lock -> chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x481do3y-1rpfqs8.lock 	deleted:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x411xmos-0ng1qes.lock 	deleted:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x412ydrp-0ivbidj.lock 	deleted:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x414ct77-11h80k9-working/dep-graph.part.bin 	deleted:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x414ct77-11h80k9.lock 	deleted:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x415pduj-09ml1th-working/dep-graph.part.bin 	deleted:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x415pduj-09ml1th.lock 	renamed:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x410p8q1-0wtogc5-working/dep-graph.part.bin -> chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x47w71vs-0o41o49-working/dep-graph.part.bin 	renamed:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x414ctv3-0266z7y.lock -> chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x47w71vs-0o41o49.lock 	renamed:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x411xmos-0ng1qes-working/dep-graph.part.bin -> chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x47zepbn-0yt9ipf-working/dep-graph.part.bin 	renamed:    chat/target/debug/incremental/client-2jjsqw2f5wap4/s-h1x415pdug-1f8odbx.lock -> chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x47zepbn-0yt9ipf.lock 	renamed:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x412ydrp-0ivbidj-working/dep-graph.part.bin -> chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x481dn4x-0xt5gd3-working/dep-graph.part.bin 	renamed:    chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x410p8q1-0wtogc5.lock -> chat/target/debug/incremental/server-2bn088hm8brrp/s-h1x481dn4x-0xt5gd3.lock

--- chat/src/utils.rs
+++ chat/src/utils.rs
@@ -5,3 +5,8 @@ use std::error::Error;
 use std::marker::Unpin;
 
 pub type ChatError = Box<dyn Error + Send + Sync + 'static>;
+pub type ChatResult<T> = Result<T, ChatError>;
+
+pub async fn send_json<O, P>(leaving: &mut 0, packet: &P) -> ChatResult<()>
+where
+    O: async_std::io::Write + Unpin,
